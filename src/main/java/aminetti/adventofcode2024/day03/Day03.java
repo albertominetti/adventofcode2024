50b3adda6c4059afc541b3e6b2e5acd3dd1dcc4e
Alberto Minetti <alberto.minetti@gmail.com>
2024-12-03 09:22:19 +0100

    day03 for AoC 2023 (part 1)
    Took 7 minutes

---
+++ src/main/java/aminetti/adventofcode2024/day03/Day03.java
@@ -0,0 +1,44 @@
+package aminetti.adventofcode2024.day03;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day03 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Day03.class);
+    private List<String> input;
+
+    public Day03() {
+    }
+
+    public void parseInput(List<String> input) {
+        this.input = input;
+
+    }
+
+    public long solvePart1() {
+        Pattern p = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+
+        long sum = 0L;
+        for (String s : input) {
+            Matcher matcher = p.matcher(s);
+
+            while (matcher.find()) {
+                Long a = Long.valueOf(matcher.group(1));
+                Long b = Long.valueOf(matcher.group(2));
+                sum += a * b;
+
+            }
+        }
+
+        return sum;
+    }
+
+    public long solvePart2() {
+
+        return 0;
+    }
+}
---
+++ src/test/java/aminetti/adventofcode2024/day03/Day03Test.java
@@ -0,0 +1,73 @@
+package aminetti.adventofcode2024.day03;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.io.IOUtils.readLines;
+import static org.apache.commons.io.IOUtils.resourceToString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class Day03Test {
+
+    @Test
+    void actualInputPart1() throws IOException {
+        // given
+        List<String> input = readLines(resourceToString("/day03/day03_input.txt", UTF_8));
+
+        // when
+        Day03 solver = new Day03();
+        solver.parseInput(input);
+        long l = solver.solvePart1();
+
+        // then
+        assertThat(l, is(179834255L));
+    }
+
+    @Test
+    void testInputPart1() throws IOException {
+        // given
+        List<String> input = readLines(resourceToString("/day03/day03_input_test.txt", UTF_8));
+
+        // when
+        Day03 solver = new Day03();
+        solver.parseInput(input);
+        long l = solver.solvePart1();
+
+        // then
+        assertThat(l, is(161L));
+    }
+
+    @Test
+    void actualInputPart2() throws IOException {
+        // given
+        List<String> input = readLines(resourceToString("/day03/day03_input.txt", UTF_8));
+
+        // when
+        Day03 solver = new Day03();
+        solver.parseInput(input);
+        long l = solver.solvePart2();
+
+        // then
+        assertThat(l, is(0L));
+    }
+
+    @Test
+    void testInputPart2() throws IOException {
+        // given
+        List<String> input = readLines(resourceToString("/day03/day03_input_test.txt", UTF_8));
+
+        // when
+        Day03 solver = new Day03();
+        solver.parseInput(input);
+        long l = solver.solvePart2();
+
+        // then
+        assertThat(l, is(0L));
+    }
+
+
+}
